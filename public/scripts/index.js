9c36b8a4561f9684ce5a4aa9d8097e8f2505c8f0
Liujiajia <liujiajia@lianfan.com>
2017-04-17 15:51:25 +0800

    添加远程url获取数据

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -2,10 +2,10 @@
  * Created by jiajia on 2017/4/17.
  */
 
-var data = [
-  {id:1, author: 'Pete Hunt', text: 'This is one comment'},
-  {id:2, author: 'Jordan', text: 'This is *another* comment'}
-];
+// var data = [
+//   {id:1, author: 'Pete Hunt', text: 'This is one comment'},
+//   {id:2, author: 'Jordan', text: 'This is *another* comment'}
+// ];
 
 var Comment = React.createClass({
   rawMarkup: function () {
@@ -51,12 +51,32 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache:false,
+      success:function (data) {
+        this.setState({data:data});
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url,status,err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function () {
+    return {data:[]}
+  },
+  componentDidMount: function () {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer,this.props.pollInterval);
+  },
   render: function () {
     return (
       <div className='commentBox'>
         Hello, world! I am a commentBox.
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
@@ -64,6 +84,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url="/api/comments" pollInterval={2000}/>,
   document.getElementById('content')
 );
